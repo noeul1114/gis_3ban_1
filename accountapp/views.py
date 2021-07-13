83b14d75d7914848bb73b06497ab52e6da8e9d5d
noeul1114 <parkhyongsok@naver.com>
2021-07-13 09:47:31 +0900

    7/13 3ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -14,8 +14,10 @@ def hello_world(request):
         model_instance.text = temp
         model_instance.save()
 
+        data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'model_instance': model_instance})
+                      context={'data_list': data_list})
     else:
+        data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'data_list': data_list})
