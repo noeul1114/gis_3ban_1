a9b73134f5a4c6944acf228b17fa03ce769e9673
noeul1114 <parkhyongsok@naver.com>
2021-07-13 09:20:33 +0900

    7/13 3ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,12 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import NewModel
 
 
 def hello_world(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'POST METHOD!'})
+                      context={'model_instance': model_instance})
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD!'})
