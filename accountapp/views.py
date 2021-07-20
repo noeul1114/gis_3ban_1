9113fb6634a87c862705a6a53522842172389715
noeul1114 <parkhyongsok@naver.com>
2021-07-20 09:18:10 +0900

    7/20 3ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import NewModel
 
@@ -34,6 +34,10 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
 
 
 
