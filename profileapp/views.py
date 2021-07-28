3c623308b7968140a0ba00f6741cd2aee9ce3bce
noeul1114 <parkhyongsok@naver.com>
2021-07-28 15:19:28 +0900

    7/28 class 3ban commit 2

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
@@ -17,6 +17,14 @@ class ProfileCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
 
 
 
